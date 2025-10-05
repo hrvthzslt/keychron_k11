8906102b9d1173e0ea9e2949a8fadbd789008fed
Zsolt Horváth <hrvth.7slt@gmail.com>
2025-10-05 20:04:41 +0200

    feat: hold on other key press per key

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/hrvthzslt/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/hrvthzslt/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT_TNAV, KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_DEL,           MA_LCK,
         MT_CTES, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_BSLS,          KC_PSCR,
         KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     MO(_FN2), KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LALT,  MT_GUBS,           KC_SPC,           MT_ALDL,  MO(_FUN),          MT_ALEN,           KC_LGUI,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LALT,  MT_GUBS,           MT_ALSP,          LT_DFUN,  MO(_FUN),          MT_ALEN,           KC_LGUI,            KC_LEFT, KC_DOWN, KC_RGHT),
 
     [BASE] = LAYOUT_69_ansi(
         KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
@@ -86,4 +86,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     process_macros(keycode, record);
     return true;
+}
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MT_ALSP:
+            // Do not select the hold action when another key is pressed.
+            return false;
+        default:
+            // Immediately select the hold action when another key is pressed.
+            return true;
+    }
 }
