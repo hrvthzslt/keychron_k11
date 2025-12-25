9fcda4c2bbeba43293ebd31034ebf48127e6e4e3
Zsolt Horváth <hrvth.7slt@gmail.com>
2025-12-25 20:31:54 +0100

    fix: use the correct fn layer on base layers

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/hrvthzslt/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/hrvthzslt/keymap.c
@@ -23,30 +23,30 @@
 #include "macros.c"
 
 enum layers{
-    MAC_BASE,
-    PC_BASE,
+    MAC_B,
+    PC_B,
     NAV,
-    MAC_FN1,
-    PC_FN1,
-    FN2,
+    MAC_F,
+    PC_F,
+    FUN,
 };
     
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [MAC_BASE] = LAYOUT_69_ansi(
+    [MAC_B] = LAYOUT_69_ansi(
         KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         LT_TNAV, KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_DEL,           MA_LCK,
         MT_CTES, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_BSLS,          KC_PSCR,
-        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     MO(PC_FN1), KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LALT,  MT_GUBS,           MT_ALSP,          LT_DFUN,  MO(FN2),          MT_ALEN,           KC_LGUI,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     MO(MAC_F),KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
+        KC_LCTL, KC_LALT,  MT_GUBS,           MT_ALSP,          LT_DFUN,  MO(FUN),           MT_ALEN,           KC_LGUI,            KC_LEFT, KC_DOWN, KC_RGHT),
 
-    [PC_BASE] = LAYOUT_69_ansi(
+    [PC_B] = LAYOUT_69_ansi(
         KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_DEL,           MA_LCK,
         KC_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_BSLS,          KC_PSCR,
-        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     MO(PC_FN1), KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LALT,  KC_LGUI,           KC_SPC,           KC_LALT,  MO(FN2),          KC_ENT,            KC_LGUI,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     MO(PC_F), KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
+        KC_LCTL, KC_LALT,  KC_LGUI,           KC_SPC,           KC_LALT,  MO(FUN),           KC_ENT,            KC_LGUI,            KC_LEFT, KC_DOWN, KC_RGHT),
 
     [NAV] = LAYOUT_69_ansi(
         KC_GRV,  KC_BRID,  KC_BRIU, KC_MCTRL, KC_LNPAD,RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
@@ -55,21 +55,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,           RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, _______,  _______, KC_PGDN, KC_HOME, C_LEFT,   C_RIGHT,  KC_END,   _______, _______,
         _______, _______,  _______,           _______,          KC_DEL,   _______,          _______,           _______,            _______, KC_PGDN, _______),
 
-    [MAC_FN1] = LAYOUT_69_ansi(
+    [MAC_F] = LAYOUT_69_ansi(
         KC_GRV,  KC_BRID,  KC_BRIU, KC_MCTRL, KC_LNPAD,RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
         _______, BT_HST1,  BT_HST2,  BT_HST3, P2P4G,   _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          KC_INS,
         RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI,           _______, _______, _______, _______,  _______,  _______,  _______,          KC_END,
         _______,           RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,  _______, NK_TOGG, _______, _______,  _______,  _______,  _______, KC_PGUP,
         _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, KC_PGDN, _______),
 
-    [PC_FN1] = LAYOUT_69_ansi(
+    [PC_F] = LAYOUT_69_ansi(
         KC_GRV,  KC_BRID,  KC_BRIU,  KC_TASK, KC_FILE, RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
         _______, BT_HST1,  BT_HST2,  BT_HST3, P2P4G,   _______, _______,  _______, _______, _______, _______,  _______,	 _______,  _______,          KC_INS,
         RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI,           _______, _______, _______, _______,  _______,  _______,  _______,          KC_END,
         _______,           RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,  _______, NK_TOGG, _______, _______,  _______,  _______,  _______, KC_PGUP,
         _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, KC_PGDN, _______),
 
-    [FN2] = LAYOUT_69_ansi(
+    [FUN] = LAYOUT_69_ansi(
         KC_TILD, KC_F1,    KC_F2,	 KC_F3,   KC_F4,   KC_F5,	KC_F6,	  KC_F7,   KC_F8,	KC_F9,	 KC_F10,   KC_F11,	 KC_F12,   _______,          _______,
         _______, _______,  _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          _______,
         _______, _______,  _______,  _______, _______, _______,           _______, _______, _______, _______,  _______,  _______,  _______,          _______,
@@ -79,12 +79,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
     const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-        [PC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+        [MAC_B] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+        [PC_B] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
         [NAV]  = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-        [MAC_BASE]  = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-        [PC_FN1]  = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-        [FN2]	   = {ENCODER_CCW_CW(_______, _______)},
+        [MAC_F]  = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+        [PC_F]  = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+        [FUN]	   = {ENCODER_CCW_CW(_______, _______)},
     };
 #endif // ENCODER_MAP_ENABLE
 
